fc67cc5f1efcdb49c2abfdfbfb049c092a25c8c8
Sebastian Laverde <lavmlk20201@gmail.com>
2022-08-24 01:27:17 +0200

    aggregator filenames order error bug fixed

--- aggregator.py
+++ aggregator.py
@@ -16,17 +16,25 @@ def read_json(path):
         return json.load(file_in)
 
 
-def save_json(data, name, ending:str):
+def save_json(data, name, ending: str):
     with open(DATA_OUT_PATH + name + ending, 'w') as f:
         json.dump(data, f)
 
 
 def extract_data_for_user(name=''):
     filenames = next(walk(DATA_IN_PATH + name), (None, None, []))[2]
+    filenames = [f for f in filenames if "DS_Store" not in f]
+    if "GazeData" in filenames[0]:
+        samples_ix = [int(s.replace(".txt", "").replace("GazeData", "")) for s in filenames]
+    else:
+        samples_ix = [int(s.replace(".txt", "").replace("PupilData", "")) for s in filenames]
+    sorter_dict = {name: ix for name, ix in zip(filenames, samples_ix)}
+    sorter_dict = dict(sorted(sorter_dict.items(), key=lambda item: item[1]))
+    filenames = sorter_dict.keys()
     return filenames
 
 
-def read_filenames_to_list_of_lists(filenames, name, field:str):
+def read_filenames_to_list_of_lists(filenames, name, field: str):
     user_data = []
     user_data_x, user_data_y = [], []
     for filename in tqdm(filenames):
@@ -62,8 +70,10 @@ def read_filenames_to_list_of_lists(filenames, name, field:str):
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--data_in", help="the path from where the raq data will be read.", default=DATA_IN_PATH)
-    parser.add_argument("-o", "--data_out", help="the path to where the data aggregation will be stored", default=DATA_OUT_PATH)
-    parser.add_argument("-f", "--field", help="the field name inside the json output from the device", choices=FIELDS, default="gaze_valid")
+    parser.add_argument("-o", "--data_out", help="the path to where the data aggregation will be stored",
+                        default=DATA_OUT_PATH)
+    parser.add_argument("-f", "--field", help="the field name inside the json output from the device", choices=FIELDS,
+                        default="gaze_valid")
     return parser.parse_args()
 
 
@@ -79,9 +89,16 @@ if __name__ == '__main__':
     # for participants names get all subdirectory names for DATA_IN_PATH...
     participants = next(walk(DATA_IN_PATH), (None, None, []))[1]
     print()
-    for participant_name in participants:
+    for ix, participant_name in enumerate(participants):
         print(participant_name)
         filenames = extract_data_for_user(participant_name)
-        x, y = read_filenames_to_list_of_lists(filenames, participant_name, args.field)
-        save_json(x, participant_name + '_x', 'coord.txt')
-        save_json(y, participant_name + '_y', 'coord.txt')
+        try:
+            x, y = read_filenames_to_list_of_lists(filenames, participant_name, args.field)
+            save_json(x, participant_name + '_x', 'coord.txt')
+            save_json(y, participant_name + '_y', 'coord.txt')
+        except ValueError:
+            data = read_filenames_to_list_of_lists(filenames, participant_name, args.field)
+            save_json(data, participant_name + 'Pupil', '.txt')
+
+
+# arif second gaze sample pattern 4 [0] is 0.273157 not 0.263006!!!
