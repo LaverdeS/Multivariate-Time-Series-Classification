149675482f5285e9b8b9400fce89d9df8149ab18
Sebastian Laverde <lavmlk20201@gmail.com>
2023-02-13 09:18:27 +0100

    feat: rocket and feature extractor methods

--- python/classifying.py
+++ python/classifying.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 
 from warnings import simplefilter
 from sktime.datatypes._panel._convert import from_nested_to_2d_array
+from sktime.transformations.panel.tsfresh import TSFreshFeatureExtractor
 from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import RidgeClassifierCV
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.dummy import DummyClassifier
@@ -20,14 +22,16 @@ class BinaryTimeSeriesClassifier(object):
     Classifier object definition. This class deals with loading the data,
     creating training sets and training/tuning the defined model/method
     """
-    def __init__(self, data_path: str, label_column: str, method: str, multi: bool = False) -> None:
+
+    def __init__(self, data_path: str, label_column: str, method: str, multi: bool = False):
         """
         Classifier constructor. It prepares the data for processing/developing from path
         """
-        self.labels = [label_column, None]
+        self.labels = [label_column, pd.DataFrame()]
         self.data = self._load_data_from_path(data_path)
         self.mode = "univariate" if not multi else "multivariate"
         self.method = method
+        self.X, self.X_name, self.y = pd.DataFrame(), '', pd.DataFrame()
         """
         Extra config/attributes can be added to set the training strategy. For instance:
         setting the classifier that the tabularization training branch will use and wether to display figure while training or not
@@ -67,6 +71,7 @@ class BinaryTimeSeriesClassifier(object):
         This methods created X and y dataframes depending if the intended use is for
         univariate or multivariate classification
         """
+        self.X_name = X_column
         if self.mode == 'univariate':
             self.X = self.data.loc[:, [X_column]]
             self.y = self.labels[1]
@@ -102,7 +107,7 @@ class BinaryTimeSeriesClassifier(object):
 
         if self.method.lower() == "tabularization":
             """
-            use a RandomForestClassifier as default, but it can be easily 
+            Use a RandomForestClassifier as default, but it can be easily 
             adapted to use ANY other classifier type after tabularization is done, including rocket.
             """
             for seed in seeds:
@@ -112,20 +117,17 @@ class BinaryTimeSeriesClassifier(object):
                 X = df_sample
                 del X['rating']
                 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
-                print("\n_____________________________________________________________________")
-
                 best_acc = 0
                 best_df_eval = pd.DataFrame()
 
                 for ix, (train_index, test_index) in enumerate(skf.split(X, y), start=1):
-                    print("\nfold: ", ix)
+                    logging.info(f"fold: {ix}")
                     X_train, X_test = X.take(train_index), X.take(test_index)
                     y_train, y_test = y.take(train_index), y.take(test_index)
                     labels, counts = np.unique(y_train, return_counts=True)
-                    print(labels, counts)
-
+                    logging.info(f"{labels, counts}")
                     n_display = 5
-                    print("displaying first {n_display} samples for each class")
+                    logging.info(f"displaying first {n_display} samples for each class")
                     fig, ax = plt.subplots(1, figsize=plt.figaspect(0.25))
 
                     for i in range(n_display):
@@ -137,18 +139,16 @@ class BinaryTimeSeriesClassifier(object):
                     ax.set(title="Fold time series first", xlabel="Time");
                     ax.get_legend().remove()
                     plt.show()
-
                     X_train_tab = from_nested_to_2d_array(X_train)
                     X_test_tab = from_nested_to_2d_array(X_test)
-
                     classifier = DummyClassifier(strategy="prior")
                     classifier.fit(X_train_tab, y_train)
-                    print("Dummy score: ", classifier.score(X_test_tab, y_test))
+                    logging.info(f"Dummy score: {classifier.score(X_test_tab, y_test)}")
 
                     classifier = RandomForestClassifier(n_estimators=100)
                     classifier.fit(X_train_tab, y_train)
                     y_pred = classifier.predict(X_test_tab)
-                    print("random forest score: ", accuracy_score(y_test, y_pred))
+                    logging.info(f"random forest score: {accuracy_score(y_test, y_pred)}")
 
                     predictions = classifier.predict(X_test_tab)
                     df_eval = pd.DataFrame([(p, t) for p, t in zip(predictions, y_test.tolist())],
@@ -157,26 +157,104 @@ class BinaryTimeSeriesClassifier(object):
                         best_acc = classifier.score(X_test_tab, y_test)
                         best_df_eval = df_eval
 
-                print("\nbest acc: ", round(best_acc, 2))
+                logging.info(f"best acc: {round(best_acc, 2)}")
                 if best_acc > best_global_acc:
                     best_global_acc = best_acc
                     best_model = best_df_eval
 
-            print("\nbest global acc: ", round(best_global_acc, 2))
-            print(classification_report(best_model.truth.tolist(), best_model.prediction.tolist(),
-                                        target_names=list(set(best_model.truth.tolist()))))
-            cm = confusion_matrix(best_model.truth.tolist(), best_model.prediction.tolist())
-            df_cm = pd.DataFrame(cm, index=list(set(best_model.truth.tolist())),
-                                 columns=list(set(best_model.truth.tolist())))
-            print("\nTABULARIZATION DATAFRAME SAMPLE: \n")
-            print(X_train_tab.head())
-            show_confusion_matrix(df_cm)
-            plt.show()
-
         elif self.method.lower() == "rocket":
+            """
+            MiniRockets train loop for ts-classification
+            """
+            logging.info(f"loadig MiniRockets! 🚀")
             from sktime.transformations.panel.rocket import MiniRocket, MiniRocketMultivariate
-            pass
+            for seed in seeds:
+                df_sample = self.data.groupby('rating', group_keys=False).apply(
+                    lambda x: x.sample(n_samples, random_state=seed))
+                y = df_sample['rating']
+                X = df_sample
+                del X['rating']
+                skf = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state=seed)
+                best_acc = 0
+                best_df_eval = pd.DataFrame()
+                logging.info(f"total number of samples in X: {len(X)}")
+                for ix, (train_index, test_index) in enumerate(skf.split(X, y), start=1):
+                    logging.info(f"fold: {ix}")
+                    logging.info(f"length a train data: {len(train_index)}")
+                    logging.info(f"length a test data: {len(test_index)}")
+                    X_train, X_test = X.take(train_index), X.take(test_index)
+                    y_train, y_test = y.take(train_index), y.take(test_index)
+
+                    minirocket = MiniRocket()  # MiniRocket() # for univariate; MiniRocketMultivariate; time series transformation
+                    minirocket.fit(X_train)
+                    X_train_transform = minirocket.transform(X_train)
+                    classifier = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True)
+                    classifier.fit(X_train_transform, y_train)
+                    X_test_transform = minirocket.transform(X_test)
+                    logging.info(f"accuracy: ", classifier.score(X_test_transform, y_test))  # mean acc using LOO
+                    logging.info(f"best_score_: ", classifier.best_score_)
+
+                    predictions = classifier.predict(X_test_transform)
+                    df_eval = pd.DataFrame([(p, t) for p, t in zip(predictions, y_test.tolist())],
+                                           columns=["prediction", "truth"])
+                    if best_acc < classifier.score(X_test_transform, y_test):
+                        best_acc = classifier.score(X_test_transform, y_test)
+                        best_df_eval = df_eval
+
+                logging.info(f"best acc: {round(best_acc, 2)}")
+                if best_acc > best_global_acc:
+                    best_global_acc = best_acc
+                    best_model = best_df_eval
+
         elif self.method.lower() == "feature-extractor":
-            pass
+            """
+            Extracts statistical features and train a RandomForestClassifier as the default model,
+            but ANY model can be used after this point
+            """
+            feature_transformer = TSFreshFeatureExtractor(default_fc_parameters="minimal")
+            extracted_features = feature_transformer.fit_transform(self.X)
+            logging.info(f"{extracted_features.describe()}")
+            extracted_features.shape
+            df_extrated_features = pd.concat([extracted_features, self.y], axis=1)
+            for seed in seeds:
+                df_sample = df_extrated_features.groupby('rating', group_keys=False).apply(
+                    lambda x: x.sample(n_samples, random_state=seed))
+                y = df_sample['rating']
+                X = df_sample
+                del X['rating']
+                skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+                best_acc = 0
+                best_df_eval = pd.DataFrame()
+                for ix, (train_index, test_index) in enumerate(skf.split(X, y), start=1):
+                    logging.info(f"fold: {ix}")
+                    X_train, X_test = X.take(train_index), X.take(test_index)
+                    y_train, y_test = y.take(train_index), y.take(test_index)
+                    classifier = DummyClassifier(strategy="prior")
+                    classifier.fit(X_train, y_train)
+                    logging.info(f"Dummy score: {classifier.score(X_test, y_test)}")
+
+                    classifier = RandomForestClassifier(n_estimators=100)
+                    classifier.fit(X_train, y_train)
+                    y_pred = classifier.predict(X_test)
+                    logging.info(f"random forest score: {accuracy_score(y_test, y_pred)}")
+                    predictions = classifier.predict(X_test)
+                    df_eval = pd.DataFrame([(p, t) for p, t in zip(predictions, y_test.tolist())],
+                                           columns=["prediction", "truth"])
+                    if best_acc < classifier.score(X_test, y_test):
+                        best_acc = classifier.score(X_test, y_test)
+                        best_df_eval = df_eval
+
+                logging.info(f"best acc: {round(best_acc, 2)}")
+                if best_acc > best_global_acc:
+                    best_global_acc = best_acc
+                    best_model = best_df_eval
         else:
             logging.warning("Available time-serie classification methods: tabularization, rocket, feature-extractor")
+        logging.info(f"best global acc: {round(best_global_acc, 2)}")
+        logging.info(
+            f"{classification_report(best_model.truth.tolist(), best_model.prediction.tolist(), target_names=list(set(best_model.truth.tolist())))}")
+        cm = confusion_matrix(best_model.truth.tolist(), best_model.prediction.tolist())
+        df_cm = pd.DataFrame(cm, index=list(set(best_model.truth.tolist())),
+                             columns=list(set(best_model.truth.tolist())))
+        show_confusion_matrix(df_cm)
+        plt.show()
